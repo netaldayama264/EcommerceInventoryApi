1d5909267e83e24dc8df14a894530cf08fbd03cc
NetalDayama <148483758+netaldayama264@users.noreply.github.com>
2026-05-21 19:39:15 +0530

    Initialize ASP.NET Core app with in-memory database
    Set up a basic ASP.NET Core application with in-memory database support and Swagger for API documentation.

---
+++ program.cs
@@ -0,0 +1,35 @@
+using EcommerceInventoryApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register the DbContext using an In-Memory Database
+builder.Services.AddDbContext<EcommerceContext>(options =>
+    options.UseInMemoryDatabase("EcommerceDb"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+// Ensure the database is created and seeded
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<EcommerceContext>();
+    context.Database.EnsureCreated();
+}
+
+app.Run();
